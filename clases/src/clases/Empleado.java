4833443ff0961527986a119eb1654044c1487402
david <david.22.mktt.davidnavarro@fundacionmontessori.com>
2025-05-16 14:03:03 +0200

    Cuarto Commit

--- clases/src/clases/Empleado.java
+++ clases/src/clases/Empleado.java
@@ -1,93 +1,104 @@
-package clases;
-
-import java.time.LocalDate;
-
-public class Empleado {
+	package clases;
 	
-	private String nombre;
-	private String puesto;
-	private double salario;
-	private LocalDate inicioContrato;
+	import java.time.LocalDate;
 	
+	public class Empleado {
+		
+		final static int NUMERO_MESES_ANYO = 12;
 	
-	public Empleado() {
-		this.nombre="Nuevo Empleado";
-		this.puesto= "Puesto predeterminado";
-		this.salario=1000;
+		private String nombre;
+		private String puesto;
+		private double salario;
+		private LocalDate inicioContrato;
 		
 		
-	}
-	
-	
-	public Empleado(String nombre, String puesto, double salario, String inicioContrato) {
-		this.nombre=nombre;
-		this.puesto=puesto;
-		this.salario=salario;
-		this.inicioContrato = LocalDate.parse(inicioContrato);
+		public Empleado() {
+			
+		}
 		
-	}
-
+		
+		public Empleado(String nombre, String puesto, double salario, String inicioContrato) {
+			this.nombre=nombre;
+			this.puesto=puesto;
+			this.salario=salario;
+			this.inicioContrato = LocalDate.parse(inicioContrato);
+			
+		}
+		
+		public void salario_anual() {
+			double sal_anual = 0;
+			
+			sal_anual = salario*NUMERO_MESES_ANYO;
+			
+			System.out.println(sal_anual);
+			
+		}
 	
+		
+		
+		
+		
 	
-	
-	
-
-	public LocalDate getInicioContrato() {
-		return inicioContrato;
-	}
-
-
-
-
-
-
-	public void setInicioContrato(LocalDate inicioContrato) {
-		this.inicioContrato = inicioContrato;
-	}
-
-
-
-
-
-
-	public String getNombre() {
-		return nombre;
-	}
-
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-
-	public String getPuesto() {
-		return puesto;
-	}
-
-
-	public void setPuesto(String puesto) {
-		this.puesto = puesto;
-	}
-
-
-	public double getSalario() {
-		return salario;
-	}
-
-
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Empleado [nombre=" + nombre + ", puesto=" + puesto + ", salario=" + salario + inicioContrato + "]";
-	}
+		public LocalDate getInicioContrato() {
+			return inicioContrato;
+		}
 	
 	
 	
 	
 	
-
-}
+	
+		public void setInicioContrato(LocalDate inicioContrato) {
+			this.inicioContrato = inicioContrato;
+		}
+	
+	
+	
+	
+	
+	
+		public String getNombre() {
+			return nombre;
+		}
+	
+	
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+	
+	
+		public String getPuesto() {
+			return puesto;
+		}
+	
+	
+		public void setPuesto(String puesto) {
+			this.puesto = puesto;
+		}
+	
+	
+		public double getSalario() {
+			return salario;
+		}
+	
+	
+		public void setSalario(double salario) {
+			this.salario = salario;
+		}
+	
+	
+		@Override
+		public String toString() {
+			return "Empleado [nombre:" + nombre + ", puesto:" + puesto + ", salario mensual:" + salario + ", inicio de contrato="
+					+ inicioContrato + "]";
+		}
+	
+	
+	
+		
+		
+		
+		
+		
+	
+	}
